7adbd45908d9aa878bbaf2b117f0ee44c06dfa05
Jonathan Keane <jkeane@gmail.com>
2020-10-09 22:38:11 -0500

    All of it?

--- tests/testthat/test-capture-requests.R
+++ tests/testthat/test-capture-requests.R
@@ -70,9 +70,10 @@ test_that("We can then load the mocks it stores", {
     expect_identical(content(m1), content(r1))
     ## Compare the HTML as text because the parsed HTML (XML) object has a
     ## C pointer that is different between the two objects.
+    expect_identical(nchar(content(r2, "text")), 1L)
     expect_identical(
-        substr(gsub("\\r", "", content(m2, "text")), 100, 1000),
-        substr(content(r2, "text"), 100, 1000)
+        substr(gsub("\\r", "", content(m2, "text")), 1000, 9591),
+        substr(content(r2, "text"), 1000, 9591)
     )
     expect_true(grepl("</body>", content(m2, "text")))
     expect_identical(content(m3), content(r3))
